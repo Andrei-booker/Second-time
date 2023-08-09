3b698bc51dffb3c84a5c9ac4d852d70b7b414b33
Andrei-booker <jep_95@mail.ru>
2023-08-09 11:28:15 +1000

    desktop and tablet button read-more

--- script.js
+++ script.js
@@ -12,3 +12,38 @@ const swiper = new Swiper('.swiper', {
       clickable: 'true',
     },
   });
+
+let buttonShowAll = document.querySelector('.button--show-all');
+let buttonHidden = document.querySelector('.buttoun--hidden');
+let tabletItems = document.querySelectorAll('.tablet-hidden');
+let desktopItems = document.querySelectorAll('.desktop-hidden');
+
+buttonShowAll.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (window.innerWidth < 1120) {
+    for (let i = 0; i < tabletItems.length; i++) {
+      tabletItems[i].classList.remove('tablet-hidden');
+    }
+  } else {
+    for (let i = 0; i < desktopItems.length; i++) {
+      desktopItems[i].classList.remove('desktop-hidden');
+    }
+  }
+  buttonHidden.classList.remove('hidden');
+  buttonShowAll.classList.add('hidden');
+});
+
+buttonHidden.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (window.innerWidth < 1120) {
+    for (let i = 0; i < tabletItems.length; i++) {
+      tabletItems[i].classList.add('tablet-hidden');
+    }
+  } else {
+    for (let i = 0; i < desktopItems.length; i++) {
+      desktopItems[i].classList.add('desktop-hidden');
+    }
+  }
+  buttonHidden.classList.add('hidden');
+  buttonShowAll.classList.remove('hidden');
+});
